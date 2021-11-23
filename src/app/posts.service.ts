4afe0ad3ea4896fe2c960cbc781b63433f30480e
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-11-23 16:20:49 +0200

    Observing Different Types of Responses

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Post} from './post.model';
-import {map, catchError} from 'rxjs/operators';
+import {map, catchError, tap} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
 
 @Injectable({
@@ -9,6 +9,7 @@ import {Subject, throwError} from 'rxjs';
 })
 export class PostsService {
   error = new Subject<string>();
+
   constructor(private http: HttpClient) {
   }
 
@@ -17,12 +18,16 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-bb822-default-rtdb.firebaseio.com/posts.json',
-        postData
-      ).subscribe(responseData => {
-      console.log(responseData);
-    }, error => {
+        postData,
+        {
+          observe: 'response'
+        }
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      }, error => {
         this.error.next(error.message);
-    });
+      });
   }
 
   fetchPosts() {
@@ -54,6 +59,21 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ng-complete-guide-bb822-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete(
+      'https://ng-complete-guide-bb822-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      })
+      .pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+          // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
