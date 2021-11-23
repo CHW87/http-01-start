c23e2f2ba2b95af455f4f35f6ec25e790f7384b2
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-11-23 14:56:02 +0200

    Using Subjects for Error Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Post} from './post.model';
 import {PostsService} from './posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub = Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {
   }
 
   ngOnInit() {
+    // @ts-ignore
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.isFetching = true;
     this.postsService.fetchPosts().subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
       }, error => {
         this.error = error.message;
-    }
+      }
     );
   }
 
@@ -40,7 +46,7 @@ export class AppComponent implements OnInit {
         this.loadedPosts = posts;
       }, error => {
         this.error = error.message;
-    }
+      }
     );
   }
 
@@ -49,5 +55,10 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    // @ts-ignore
+    this.errorSub.unsubscribe();
   }
 }
