bdf94b1671558d42b43c98211210d8771a91ccc0
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-11-23 14:28:46 +0200

    Handling Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {PostsService} from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {
   }
@@ -20,7 +21,9 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
-      }
+      }, error => {
+        this.error = error.message;
+    }
     );
   }
 
@@ -35,7 +38,9 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
-      }
+      }, error => {
+        this.error = error.message;
+    }
     );
   }
 
